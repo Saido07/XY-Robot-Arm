d6baffa7342a6875373517ebe6e0cef4438270b2
sait <e160501132@stud.tau.edu.tr>
2021-05-14 15:00:42 +0300

    deneme

---
+++ .qt_for_python/uic/form.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.4
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class Ui_main(object):
+    def setupUi(self, main):
+        if not main.objectName():
+            main.setObjectName(u"main")
+        main.resize(800, 600)
+        self.centralwidget = QWidget(main)
+        self.centralwidget.setObjectName(u"centralwidget")
+        main.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(main)
+        self.menubar.setObjectName(u"menubar")
+        main.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(main)
+        self.statusbar.setObjectName(u"statusbar")
+        main.setStatusBar(self.statusbar)
+
+        self.retranslateUi(main)
+
+        QMetaObject.connectSlotsByName(main)
+    # setupUi
+
+    def retranslateUi(self, main):
+        main.setWindowTitle(QCoreApplication.translate("main", u"main", None))
+    # retranslateUi
+
