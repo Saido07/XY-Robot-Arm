178533e93e7d0da73c2ed86d29a6e42628cac055
sait <e160501132@stud.tau.edu.tr>
2021-06-19 11:26:44 +0300

    Bilinen hatalar giderildi, GCode dönüşümü eklendi.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from typing import overload
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
-
+import __init__
+import gOut
+import gRead
 from PyQt5 import QtGui, QtWidgets, uic
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
@@ -20,13 +22,14 @@ import cv2
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QImage, QPalette, QBrush
 import numpy as np
-import imutils
+import utlis
 import serial
 import time
 import argparse
 import serial.tools.list_ports
 import winreg
 import itertools
+import imutils
 from skimage.filters import threshold_local
 from imutils.perspective import four_point_transform
 
@@ -59,7 +62,7 @@ class main(QMainWindow):
         call.refreshPorts.setIconSize(size)
         call.refreshPorts.setStyleSheet("background:transparent")
         self.serial_ports()
-        self.Port=self.comboBox.currentText()
+        self.refreshPortClicked()
 
     def refreshPortClicked(self):
         self.serial_ports()
@@ -68,17 +71,18 @@ class main(QMainWindow):
     def serial_ports(self) -> list:
         self.comboBox.clear()
         path = 'HARDWARE\DEVICEMAP\SERIALCOMM'
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+        
 
         ports = []
 
         for i in itertools.count():
             try:
+                key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
                 ports.append(winreg.EnumValue(key, i)[1])
                 self.comboBox.addItem(winreg.EnumValue(key, i)[1])
             except EnvironmentError:
                 break
-           # print(ports)
+        # print(ports)
         return ports
 
 
@@ -92,76 +96,115 @@ class main(QMainWindow):
         print("camClicked")
         self.infoScreen.setText(self.infoScreen.text()+"\n--Die Kamera war eingeschaltet.")  
         self.logic=0
-        count=0
+        utlis.initializeTrackbars()
+        count = 0
         self.oriImage.setVisible(True)
         self.workedImage.setVisible(True)
         self.camOn.setVisible(False)
         self.sendGcode.setVisible(False)
         self.takePhoto.setVisible(True)
         print("Cam ON")
-        cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        widthImg = 400
+        heightImg = 400
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        cap.set(10, 160)
+        
         kernel = np.ones((5,5), np.uint8)
 
         while True:
-           
-            ret, img = cam.read()
-            gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(gray_frame, (5,5), 0)
-            canny = cv2.Canny(blur , 70, 200)
-            cv2.waitKey()
+            success, img = cap.read()
+            img = cv2.resize(img, (widthImg, heightImg))  # RESIZE IMAGE
+            imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8)  # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+            imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # CONVERT IMAGE TO GRAY SCALE
+            imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  # ADD GAUSSIAN BLUR
+            thres = utlis.valTrackbars()  # GET TRACK BAR VALUES FOR THRESHOLDS
+            imgThreshold = cv2.Canny(imgBlur, thres[0], thres[1])  # APPLY CANNY BLUR
+            kernel = np.ones((5, 5))
+            imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)  # APPLY DILATION
+            imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+            imgWarpColored=None
 
-            cnts = cv2.findContours(canny.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = imutils.grab_contours(cnts)
-            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+            ## FIND ALL COUNTOURS
+            imgContours = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
+            imgBigContour = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
+            contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL,
+                                                cv2.CHAIN_APPROX_SIMPLE)  # FIND ALL CONTOURS
+            cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)  # DRAW ALL DETECTED CONTOURS
 
-            for c in cnts:
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.03 * peri, True)
-                if len(approx) == 4:
-                    screenCnt = approx
-                    break
-            normal=-1
-            k=cv2.waitKey(1)
-            try:
-                ori=cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 4)
-                #cv2.imshow("Die Kanten", frame_resized_3)
-                warped = four_point_transform(img, screenCnt.reshape(4, 2))
-                warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-                T = threshold_local(warped, 11, offset=10, method="gaussian")
-                warped = (warped > T).astype("uint8") * 255
-                self.displayImage(self.workedImage,imutils.resize(warped, 400,400),1)
-                self.displayImage(self.oriImage,ori,1)
-                normal=0
-            except:
-                self.displayImage(self.oriImage,img,1)
-                self.displayImage(self.workedImage,canny,1)
-                normal=1
+            # FIND THE BIGGEST COUNTOUR
+            biggest, maxArea = utlis.biggestContour(contours)  # FIND THE BIGGEST CONTOUR
+            if biggest.size != 0:
+                biggest = utlis.reorder(biggest)
+                cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)  # DRAW THE BIGGEST CONTOUR
+                imgBigContour = utlis.drawRectangle(imgBigContour, biggest, 2)
+                pts1 = np.float32(biggest)  # PREPARE POINTS FOR WARP
+                pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])  # PREPARE POINTS FOR WARP
+                matrix = cv2.getPerspectiveTransform(pts1, pts2)
+                imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
-            if not ret:
+                # REMOVE 20 PIXELS FORM EACH SIDE
+                imgWarpColored = imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+                imgWarpColored = cv2.resize(imgWarpColored, (widthImg, heightImg))
+
+                # APPLY ADAPTIVE THRESHOLD
+                imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+                imgAdaptiveThre = cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+                imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+                imgAdaptiveThre = cv2.medianBlur(imgAdaptiveThre, 3)
+
+                # Image Array for Display
+                imageArray = ([imgContours])
+                imageArray2 = ([imgWarpColored])
+
+            else:
+                imageArray = ([imgContours])
+                imageArray2= ([imgBlank])
+
+            # LABELS FOR DISPLAY
+            lables = [["Original", "Gray", "Threshold", "Contours"],
+                    ["Biggest Contour", "Warp Prespective", "Warp Gray", "Adaptive Threshold"]]
+
+            stackedImage = utlis.stackImages(imageArray, 1)
+            stackedImage2 = utlis.stackImages(imageArray2, 1)
+            #cv2.imshow("Result", stackedImage)
+            self.displayImage(self.oriImage,stackedImage[:400],1)
+            self.displayImage(self.workedImage,stackedImage2[:400],1)
+            #self.displayImage(self.oriImage,ori,1)
+
+            if not success:
                 break
 
-
+        
             if self.logic==2:
                 print("Cam OFF")
                 self.camOn.setVisible(True)
                 self.sendGcode.setVisible(True)
                 self.takePhoto.setVisible(False)
-                break                
-            
-            if self.logic==1:
-                self.infoScreen.setText(self.infoScreen.text()+"\n--Das Foto wurde gemacht.") 
-                if normal==0:
-                    cv2.imwrite('myImage.png',imutils.resize(warped, 400,400))
-                    self.displayImage(self.oriImage,cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 4),1)
-                    self.displayImage(self.workedImage,cv2.imread('myImage.png'),1)
-                elif normal==1:
-                    cv2.imwrite('myImage.png',canny)
-                    self.displayImage(self.oriImage,img,1)
-                    self.displayImage(self.workedImage,cv2.imread('myImage.png'),1)
+                break 
+
+            # SAVE IMAGE WHEN 's' key is pressed
+            if cv2.waitKey(1) & self.logic==1:
+                try:
+                    cv2.imwrite("Scanned/myImage.png", imgWarpColored)
+                    cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                                (1100, 350), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                                cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+                    self.infoScreen.setText(self.infoScreen.text()+"\n--Das Foto wurde gemacht.")
+                    #cv2.imshow('Result', stackedImage)
+                    cv2.waitKey(300)
+
+                    if os.path.isfile("Scanned/myImage.png"):
+                        paths = gRead.imToPaths("Scanned/myImage.png")
+                        outfile = "Scanned/myImage.gcode"
+                        gOut.toTextFile(outfile, paths)
+                        self.infoScreen.setText(self.infoScreen.text()+"\n--G-Code wurde erstellt.") 
+                    count += 1
+                except:
+                    self.infoScreen.setText(self.infoScreen.text()+"\n--Das Dokument konnte nicht bestimmt werden.")
+                    self.infoScreen.setText(self.infoScreen.text()+"\n--Bitte versuche es erneut!")
                 self.logic=2
-                normal=-1
-        
-        cam.release() 
+        cap.release() 
         for i in range(1,10):
             cv2.destroyAllWindows()
             cv2.waitKey(1)
@@ -169,7 +212,6 @@ class main(QMainWindow):
         self.sendGcode.setVisible(True)
         self.oriImage.setVisible(True)
         self.logic=0 
-        screenCnt=None
     
     def displayImage(self,lbl, img,window=1):
         qformat=QImage.Format_Indexed8
@@ -192,11 +234,13 @@ class main(QMainWindow):
 
     def sendGClicked(self):
         self.infoScreen.setText(self.infoScreen.text()+"\n--Der G-Code-Sendevorgang wurde gestartet und Druckvorgang wurde gestartet.\n--\n--\n--")
+        self.Port=self.comboBox.currentText()
         self.sendG(self.Port.lower(), "gCodes\deneme.g")
 
     
     def sendG(self, com, file):
         print("sendGCodeClicked")
+        print("COM : " , com)
         parser = argparse.ArgumentParser(description='This is a basic gcode sender. http://crcibernetica.com')
         #parser.add_argument('-p','--port',help='Input USB port',required=True)
         #parser.add_argument('-f','--file',help='Gcode file name',required=True)
@@ -211,7 +255,7 @@ class main(QMainWindow):
         # Open serial port
         #s = serial.Serial('/dev/ttyACM0',115200)
         try:
-            s = serial.Serial(args.port,9600)
+            s = serial.Serial(args.port, 115200)
             # Open g-code file
             #f = open('/media/UNTITLED/shoulder.g','r');
             try:
