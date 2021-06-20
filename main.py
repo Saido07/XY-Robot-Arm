9de520d4a43f41059bac46725aac74fe1b73a476
sait <e160501132@stud.tau.edu.tr>
2021-06-20 12:56:30 +0300

    Donma Hatası Giderildi

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# This Python file uses the following encoding: utf-8
 import os
 from pathlib import Path
 import sys
@@ -10,14 +9,13 @@ from PyQt5.QtCore import *
 import __init__
 import gOut
 import gRead
-from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5 import uic
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QImage,QPixmap
 from PyQt5.QtWidgets import  QWidget, QMainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QFile
-from PyQt5 import QtWidgets, uic
 import cv2
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QImage, QPalette, QBrush
@@ -29,8 +27,7 @@ import serial.tools.list_ports
 import winreg
 import utlis
 import itertools
-from skimage.filters import threshold_local
-from imutils.perspective import four_point_transform
+import threading
 
 
 class main(QMainWindow):
@@ -89,7 +86,6 @@ class main(QMainWindow):
                 self.comboBox.addItem(winreg.EnumValue(key, i)[1])
             except EnvironmentError:
                 break
-        # print(ports)
         return ports
 
 
@@ -129,45 +125,40 @@ class main(QMainWindow):
 
         while True:
             success, img = cap.read()
-            img = cv2.resize(img, (widthImg, heightImg))  # RESIZE IMAGE
-            imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8)  # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
-            imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # CONVERT IMAGE TO GRAY SCALE
-            imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  # ADD GAUSSIAN BLUR
-            imgThreshold = cv2.Canny(imgBlur, self.bar1.value(), self.bar2.value())  # APPLY CANNY BLUR
+            img = cv2.resize(img, (widthImg, heightImg))  
+            imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8) 
+            imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+            imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  
+            imgThreshold = cv2.Canny(imgBlur, self.bar1.value(), self.bar2.value())  
             kernel = np.ones((5, 5))
-            imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)  # APPLY DILATION
-            imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+            imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) 
+            imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  
             imgWarpColored=None
 
-            ## FIND ALL COUNTOURS
-            imgContours = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
-            imgBigContour = img.copy()  # COPY IMAGE FOR DISPLAY PURPOSES
+            imgContours = img.copy() 
+            imgBigContour = img.copy() 
             contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL,
-                                                cv2.CHAIN_APPROX_SIMPLE)  # FIND ALL CONTOURS
-            cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)  # DRAW ALL DETECTED CONTOURS
+                                                cv2.CHAIN_APPROX_SIMPLE)  
+            cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) 
 
-            # FIND THE BIGGEST COUNTOUR
-            biggest, maxArea = utlis.biggestContour(contours)  # FIND THE BIGGEST CONTOUR
+            biggest, maxArea = utlis.biggestContour(contours)  
             if biggest.size != 0:
                 biggest = utlis.reorder(biggest)
-                cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)  # DRAW THE BIGGEST CONTOUR
+                cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)  
                 imgBigContour = utlis.drawRectangle(imgBigContour, biggest, 2)
-                pts1 = np.float32(biggest)  # PREPARE POINTS FOR WARP
-                pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])  # PREPARE POINTS FOR WARP
+                pts1 = np.float32(biggest) 
+                pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]]) 
                 matrix = cv2.getPerspectiveTransform(pts1, pts2)
                 imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
 
-                # REMOVE 20 PIXELS FORM EACH SIDE
                 imgWarpColored = imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
                 imgWarpColored = cv2.resize(imgWarpColored, (widthImg, heightImg))
 
-                # APPLY ADAPTIVE THRESHOLD
                 imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
                 imgAdaptiveThre = cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
                 imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
                 imgAdaptiveThre = cv2.medianBlur(imgAdaptiveThre, 3)
 
-                # Image Array for Display
                 imageArray = ([imgContours])
                 imageArray2 = ([imgWarpColored])
 
@@ -175,16 +166,13 @@ class main(QMainWindow):
                 imageArray = ([imgContours])
                 imageArray2= ([imgBlank])
 
-            # LABELS FOR DISPLAY
             lables = [["Original", "Gray", "Threshold", "Contours"],
                     ["Biggest Contour", "Warp Prespective", "Warp Gray", "Adaptive Threshold"]]
 
             stackedImage = utlis.stackImages(imageArray, 1)
             stackedImage2 = utlis.stackImages(imageArray2, 1)
-            #cv2.imshow("Result", stackedImage)
             self.displayImage(self.oriImage,stackedImage[:400],1)
             self.displayImage(self.workedImage,stackedImage2[:400],1)
-            #self.displayImage(self.oriImage,ori,1)
 
             if not success:
                 break
@@ -201,7 +189,6 @@ class main(QMainWindow):
                 self.t2.setVisible(False)
                 break 
 
-            # SAVE IMAGE WHEN 's' key is pressed
             if cv2.waitKey(1) & self.logic==1:
                 try:
                     cv2.imwrite("Scanned/myImage.png", imgWarpColored)
@@ -210,7 +197,6 @@ class main(QMainWindow):
                     cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
                                 cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
                     self.infoScreen.setText(self.infoScreen.text()+"\n--Das Foto wurde gemacht.")
-                    #cv2.imshow('Result', stackedImage)
                     cv2.waitKey(300)
 
                     if os.path.isfile("Scanned/myImage.png"):
@@ -227,7 +213,6 @@ class main(QMainWindow):
         for i in range(1,10):
             cv2.destroyAllWindows()
             cv2.waitKey(1)
-        #self.imgLabel.setVisible(False)
         self.sendGcode.setVisible(True)
         self.oriImage.setVisible(True)
         self.logic=0 
@@ -252,55 +237,57 @@ class main(QMainWindow):
         self.logic=1
 
     def sendGClicked(self):
+
         self.infoScreen.setText(self.infoScreen.text()+"\n--Der G-Code-Sendevorgang wurde gestartet und Druckvorgang wurde gestartet.\n--\n--\n--")
         self.Port=self.comboBox.currentText()
-        self.sendG(self.Port.lower(), "Scanned\myImage.gcode")
+        parser = argparse.ArgumentParser(description='This is a basic gcode sender. http://crcibernetica.com')
+        args = parser.parse_args()
+        args.port=self.Port.lower()
+        args.file="Scanned\myImage.gcode"
+        thread_svg=threading.Thread(target=self.sendG,args=(args.port, args.file),daemon=True)
+        thread_svg.start()
 
     
     def sendG(self, com, file):
         print("sendGCodeClicked")
         print("COM : " , com)
-        parser = argparse.ArgumentParser(description='This is a basic gcode sender. http://crcibernetica.com')
-        #parser.add_argument('-p','--port',help='Input USB port',required=True)
-        #parser.add_argument('-f','--file',help='Gcode file name',required=True)
-        args = parser.parse_args()
-        args.port=com
-        args.file=file
-        ## show values ##
-        self.infoScreen.setText(self.infoScreen.text()+"\n--USB Port: %s" % args.port)
-        self.infoScreen.setText(self.infoScreen.text()+"\n--Gcode file: %s" % args.file)
-        print ("USB Port: %s" % args.port )
-        print ("Gcode file: %s" % args.file )
-        # Open serial port
-        #s = serial.Serial('/dev/ttyACM0',115200)
+        self.infoScreen.setText(self.infoScreen.text()+"\n--USB Port: %s" % com)
+        self.infoScreen.setText(self.infoScreen.text()+"\n--Gcode file: %s" % file)
+        print ("USB Port: %s" % com )
+        print ("Gcode file: %s" % file )
         try:
-            s = serial.Serial(args.port, 115200)
-            # Open g-code file
-            #f = open('/media/UNTITLED/shoulder.g','r');
+            s = serial.Serial(com, 115200)
             try:
-                f = open(args.file,'r')
-                time.sleep(2)   # Wait for Printrbot to initialize
-                s.flushInput()  # Flush startup text in serial input
+                f = open(file,'r')
+                time.sleep(2)  
+                s.flushInput()  
                 a=True
                 for line in f:
                     l = self.removeComment(line)
-                    l = l.strip() # Strip all EOL characters for streaming
+                    l = l.strip() 
                     if a==True:
-                        self.infoScreen.setText(self.infoScreen.text()+"\n--Druckvorgang wurde gestartet.")
+                        self.infoScreen.setText(self.infoScreen.text()+"\n--Druckvorgang wurde gestartet...")
+                        self.sendGcode.setEnabled(False)
+                        self.camOn.setEnabled(False)
                         a=False
                     print(l)
                     if  (l.isspace()==False and len(l)>0) :
-                        s.write((l + '\n').encode()) # Send g-code block
-                        grbl_out = s.readline() # Wait for response with carriage return
+                        s.write((l + '\n').encode()) 
+                        grbl_out = s.readline() 
                         print(grbl_out)
                 self.infoScreen.setText(self.infoScreen.text()+"\n--Druckvorgang ist fertig.")   
-                # Close file and serial port
+                self.sendGcode.setEnabled(True)
+                self.camOn.setEnabled(True)
                 f.close()
                 s.close()
             except:
+                self.sendGcode.setEnabled(True)
+                self.camOn.setEnabled(True)
                 self.infoScreen.setText(self.infoScreen.text()+"\n--Das G-Code-Verzeichnis ist falsch oder der es wurde nicht gefunden.")   
                 print("--Das G-Code-Verzeichnis ist falsch oder der es wurde nicht gefunden.")
         except:
+            self.sendGcode.setEnabled(True)
+            self.camOn.setEnabled(True)
             self.infoScreen.setText(self.infoScreen.text()+"\n--Der Zeichenroboter ist nicht angeschlossen oder es ist an den falschen Anschluss angeschlossen.")  
             print("arduino bağlanmadı ya da arduino yanlış porta bağlı.")
 
